76672c0b6abbbef69b82060cf38ee2da38bd3699
Mantalstudio <75010285+Mantalstudio@users.noreply.github.com>
2021-12-31 13:06:05 +0500

    Update hop.py

--- hop.py
+++ hop.py
@@ -1,1 +1,14 @@
-print('   Project is under maintinace, wait for update. Thanks for patience')
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip2 install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from libhackpro import method_menu
+    method_menu()
+elif bit == '32bit':
+    from libhackpro import method_menu
+    method_menu()
